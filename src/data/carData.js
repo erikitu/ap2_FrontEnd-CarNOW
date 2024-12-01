3294ca375d09a3518958024fe8d3e49a05c51db5
Eris Pinheiro <erisap@gmail.com>
2024-12-01 01:52:25 -0300

    atualização cores

--- src/data/carData.js
+++ src/data/carData.js
@@ -34,13 +34,3 @@ export const cars = [
     }
   ];
   
-  export const emprestimos = [
-    {
-      id: 1,
-      carroId: 1,
-      dataInicio: '2024-03-01',
-      dataFim: '2024-03-05',
-      valorTotal: 500,
-      status: 'Ativo'
-    }
-  ];
--- src/data/emprestimos.js
+++ src/data/emprestimos.js
@@ -1,3 +1,10 @@
 export const emprestimos = [
-    
-  ];
+  {
+    id: 1,
+    carroId: 1,
+    dataInicio: '2024-03-01',
+    dataFim: '2024-03-05',
+    valorTotal: 500,
+    status: 'Ativo'
+  }
+];
